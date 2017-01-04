d213e16952fc73be9ac31e3c03e65925298a81cc
Fevax <fer.vonarx@gmail.com>
2017-01-04 03:16:57 -0300

    Move to LineageOS

--- cm.mk
+++ cm.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_herolte
+PRODUCT_NAME := lineage_herolte
 PRODUCT_DEVICE := herolte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
