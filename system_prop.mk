0ee7969ef209596b562887ef4ae811b17a25cea2
Jesse Chan <jc@linux.com>
2017-01-05 20:37:29 +0800

    herolte: sync with hero2lte

--- system_prop.mk
+++ system_prop.mk
@@ -93,6 +93,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.use_data_netmgrd=false \
     persist.data.netmgrd.qos.enable=false \
     ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10 \
-    ro.cbd.dt_revision=008 \
-    hw.revision=8
+    ro.ril.gprsclass=10
