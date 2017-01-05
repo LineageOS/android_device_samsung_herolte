033857154ba17cbf6e12d5b62c64d08e069d8488
Jesse Chan <jc@linux.com>
2017-01-05 20:37:29 +0800

    herolte: sync with hero2lte

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_herolte-eng
-add_lunch_combo lineage_herolte-userdebug
-add_lunch_combo lineage_herolte-user
+add_lunch_combo cm_herolte-eng
+add_lunch_combo cm_herolte-userdebug
+add_lunch_combo cm_herolte-user
