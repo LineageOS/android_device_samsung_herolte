dae904a06190ad605528ce16890c23b5814c670c
Jesse Chan <jc@lineageos.org>
2017-02-10 21:12:53 +0800

    hero: move common properties to common repo
    Change-Id: I0992e57cc8650ae33c305bf293c166f32b1708e4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,23 +17,6 @@
 TARGET_KERNEL_SOURCE := kernel/samsung/universal8890
 TARGET_KERNEL_CONFIG := exynos8890-herolte_defconfig
 
-# DT/Boot/Recovery Image
-TARGET_DTS_NAMES := exynos8890-herolte_eur_open
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-
-# Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE     := 41943040
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 48234496
-BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 4508876800
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 27044851712
-BOARD_CACHEIMAGE_PARTITION_SIZE    := 209715200
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-TARGET_RECOVERY_FSTAB := device/samsung/hero-common/ramdisk/fstab.samsungexynos8890
-
-# RIL
-BOARD_MODEM_TYPE := ss333
-
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := heroltebmc,herolteskt,heroltektt,heroltelgt,heroltexx,herolte
 
