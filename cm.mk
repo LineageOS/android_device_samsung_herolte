LineageOS/android_device_samsung_hero2lte

Revert "[TEMP] Revert "Move to LineageOS""
This reverts commit 4b426b106702b61f3b935aac372fe6b84048515e.

cm.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_hero2lte
+PRODUCT_NAME := lineage_hero2lte
 PRODUCT_DEVICE := hero2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung

vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo cm_hero2lte-eng
-add_lunch_combo cm_hero2lte-userdebug
-add_lunch_combo cm_hero2lte-user
+add_lunch_combo lineage_hero2lte-eng
+add_lunch_combo lineage_hero2lte-userdebug
+add_lunch_combo lineage_hero2lte-user

LineageOS/android_device_samsung_herolte

cm.mk
@@ -29,7 +29,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_herolte
+PRODUCT_NAME := lineage_herolte
 PRODUCT_DEVICE := herolte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung

vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo cm_herolte-eng
-add_lunch_combo cm_herolte-userdebug
-add_lunch_combo cm_herolte-user
+add_lunch_combo lineage_herolte-eng
+add_lunch_combo lineage_herolte-userdebug
+add_lunch_combo lineage_herolte-user