4e8cbad9ab1cb1850c2a1c69851719a5230ca026
Stricted <info@stricted.net>
2018-09-23 02:51:14 +0200

    herolte: add bluetooth configuration
    Change-Id: Iebcb4132923f6c280a697aeb3f70afd19009f328

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,9 @@ include device/samsung/universal8890-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/samsung/herolte
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2020-04-20
 
