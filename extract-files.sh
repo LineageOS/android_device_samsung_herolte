2c0894f97fe0310858c021c68d3dbb8f60d55285
Michael Bestas <mkbestas@lineageos.org>
2020-11-05 19:50:53 +0200

    herolte: Switch to patchelf from tools-lineage
    Change-Id: If1a7328804fb9bc39ccec70690f1090f2d0509b3

--- extract-files.sh
+++ extract-files.sh
@@ -69,7 +69,7 @@ extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
 BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
 # Replace protobuf with vndk29 compat libs for specified libs
-patchelf --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/libMcClient.so
-patchelf --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib64/libMcClient.so
+"${PATCHELF}" --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/libMcClient.so
+"${PATCHELF}" --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib64/libMcClient.so
 
 "${MY_DIR}/setup-makefiles.sh"
