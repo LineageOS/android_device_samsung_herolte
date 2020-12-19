010c0a4194efd3e5377f80d5fe63ca51034852af
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    herolte: Switch to standalone extract utils
    * Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 #
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-VENDOR=samsung
 DEVICE=herolte
+VENDOR=samsung
 
 export INITIAL_COPYRIGHT_YEAR=2017
 
@@ -26,9 +16,9 @@ export INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -36,10 +26,10 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
 # Copyright headers and guards
-write_headers "herolte"
+write_headers
 
 # The standard blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
